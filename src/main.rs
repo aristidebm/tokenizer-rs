39995f131523d4f42d5dd16da8a1bf440e1a12cc
Aristide <aristide.bamazi@imsp-uac.org>
2026-04-25 21:18:27 +0000

    scafold the lexer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+#[derive(Debug, Clone)]
+enum TokenType {
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+    LBracket,
+    RBracket,
+    Whitespace,
+    EOF,
+}
+
+#[derive(Debug, Clone)]
+struct Pos {
+    line: u32,
+    col: u32,
+}
+
+#[derive(Debug, Clone)]
+struct Token {
+    value: String,
+    token_type: TokenType,
+    pos: Pos,
+}
+
+#[derive(Debug, Default)]
+struct Tokenizer {
+    source: String,
+}
+
+impl Iterator for Tokenizer {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let token = Token {
+            value: "(".to_string(),
+            token_type: TokenType::LParen,
+            pos: Pos { line: 1, col: 1 },
+        };
+        Some(token)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut lexer = Tokenizer {
+        source: "".to_string(),
+    };
+    println!("{:?}", lexer.next().unwrap());
 }
